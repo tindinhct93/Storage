5db0b69bb5825a85acf82d12ff2b7f6f0a5c4f90
tindinhct93 <tindinhct93@gmail.com>
2023-12-24 01:33:01 +0000

    Fix CORS

--- backend/api/routes/LoadReportRoute.go
+++ backend/api/routes/LoadReportRoute.go
@@ -8,12 +8,11 @@ import (
 
 func LoadReportRoute(route *gin.RouterGroup, db *mongo.Client) {
 	reportHandler := handlers.NewReportHandler(db)
-	route.GET("/", reportHandler.GetReportsByFilter)
-	route.POST("/", reportHandler.CreateReportFromExcel)
+	route.GET("", reportHandler.GetReportsByFilter)
+	route.POST("", reportHandler.CreateReportFromExcel)
 	route.POST("/QA/:id", reportHandler.UpdateQAReceiveForReport)
 	route.POST("/Borrow/:id", reportHandler.BorrowReport)
 	route.POST("/Return/:id", reportHandler.ReturnReport)
 	route.POST("/Boxes/:id", reportHandler.BoxAboveReportInNewBox)
 	route.POST("/UnBoxes/:id", reportHandler.DeleteBox)
-
 }
--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -16,15 +15,11 @@ func main() {
 	r := gin.Default()
 
 	env := godotenv.Load()
-
-	//config := cors.DefaultConfig()
-	//config.AllowOrigins = []string{"http://localhost:4000"}
-
-	r.Use(cors.Default())
-
 	if env != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	r.Use(CORSMiddleware())
 
 	monggodb := os.Getenv("MONGODB_PATH")
 	if monggodb == "" {
@@ -37,4 +32,21 @@ func main() {
 	}
 	routes.LoadRoutes(r, db)
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
